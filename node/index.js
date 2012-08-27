463032a21e381137af7a1b3d1138c6962435a573
PerterPon <PerterPon@gmail.com>
2012-08-27 22:06:05 +0800

    node读文件还是有些问题

--- node/index.js
+++ node/index.js
@@ -1,13 +1,15 @@
-﻿var fs   = require('fs'),
-	http = require('http'),
-	webSocket = require('ws'), 
-	wss  = creatHttpServer();
+﻿var fs   = require( 'fs' ),
+	http = require( 'http' ),
+	webSocket = require( 'ws' ), 
+	wss  = createHttpServer(),
+	socket;
 	
 wss.on('connection', function( ws ) {
-	console.log('connected');
-	ws.on('message', onmessage);
-	ws.on('close', function() {
-		console.log('stopping client');
+	console.log( 'connected' );
+	socket = ws;
+	ws.on( 'message', onmessage );
+	ws.on( 'close', function() {
+		console.log( 'stopping client' );
 	});
 });
 
@@ -15,27 +17,53 @@ var nameTable = {
 	"testStore" : "Data/test.json"
 }
 
-function creatHttpServer(){
+function createHttpServer(){
 	var app = http.createServer( '' ).listen( 4239 ),
 		WebSocketServer = webSocket.Server,
 		wss = new WebSocketServer( { server : app } );
 	return wss;
 }
 
-function onmessage(buffer, flags){
-	var buffer = JSON.parse(buffer),
-		name   = buffer.storeName,
-		data   = buffer.data,
-		dataBuffer = JSON.stringify(data);
-	fs.writeFile(nameTable[name], dataBuffer, function(err){
-		if(err){
+function onmessage( buffer, flags ){
+	var buffer = JSON.parse( buffer ),
+		name   = buffer.storeName;
+	if( !name ){
+		socket.send( 'store name is empty!' );
+		throw 'store name is empty!';
+	}
+	var data   = buffer.data,
+		oprtn  = buffer.operation,
+		dataBuffer = JSON.stringify( data );
+	if( oprtn == 'get' ){
+		doGetData( name );
+	} else if( oprtn == 'save' ){
+		doSaveData( name, dataBuffer );
+	}
+}
+
+function doGetData( name ){
+	var result;
+	fs.readFile( nameTable[name], function( err, data ){
+		if( err )
 			throw err;
-		}
-		console.log("Store: " + name);
-		console.log("data: " + dataBuffer);
+		result = JSON.stringify( data );
+		console.log( 'GET' );
+		console.log( 'store: ' + name );
+		console.log( 'result: ' + result );
+		socket.send( result );
+	});
+	
+}
+
+function doSaveData( name, dataBuffer ){
+	fs.writeFile( nameTable[name], dataBuffer, function( err ){
+		if(err)
+			throw err;
+		console.log( 'SAVE' );
+		console.log( 'Store:' + name );
+		console.log( 'data:'  + dataBuffer );
+		socket.send( 'succeed!' );
 	});
 }
 
-function doGetData(){
-	
-}
+
