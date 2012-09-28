09b462b1ecdf23f7f49bbbb70b512daa86fb2103
PerterPon <PerterPon@gmail.com>
2012-09-28 16:36:07 +0800

    修改Topbar定高20px

--- script/model/BaseIOS.js
+++ script/model/BaseIOS.js
@@ -2,8 +2,18 @@
 define( function( require, exports, module ){
     "use strick";
 
+    require( 'BaseModel' );
     Ext.define( 'BaseIOS', {
+        extend : 'BaseModel'
 
+        constructor : function( cfg ){
+            this.callParent([ cfg ]);
+
+        },
+        
+        __initBackground : function(){
+            
+        }
     });
 
     return BaseIOS;
